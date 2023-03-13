9198f5885d94b89c924659359ab27f9f8d5a73f3
Paulo Gustavo <pauloestudando159@gmail.com>
2023-03-13 10:28:49 -0300

    inicio do projeto

---
+++ src/main/java/com/consumo/api/tempo/ConsumoApiTempoApplication.java
@@ -0,0 +1,13 @@
+package com.consumo.api.tempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoApiTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoApiTempoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consumo/api/tempo/ConsumoApiTempoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumo.api.tempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoApiTempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
